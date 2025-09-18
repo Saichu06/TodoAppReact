92522e827b04aa86ea78216bd7e18a300caea546
Saicharan <assaicharan06@gmail.com>
2025-09-18 23:13:37 +0530

    Commit - 2

---
+++ todo-project/src/components/TodoContainer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Todo from './Todo'
+
+function TodoContainer({ todos, deleteTodo }) {
+  return (
+    <div className="container">
+      {todos.map((todo, index) => (
+        <Todo 
+          key={index} 
+          todo={todo} 
+          index={index}   // ✅ pass index here
+          deleteTodo={deleteTodo} 
+        />
+      ))}
+    </div>
+  );
+}
+
+export default TodoContainer;
